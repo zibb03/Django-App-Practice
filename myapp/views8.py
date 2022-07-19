499ad4eead31f3018b8f0371413acff83864a9ee
zibb03 <64014435+zibb03@users.noreply.github.com>
2022-07-20 03:43:39 +0900

    9회차 form 태그

--- myapp/views8.py
+++ myapp/views8.py
@@ -22,13 +22,9 @@ def HTMLTemplate(articleTag):
             {ol}
         </ul>
         {articleTag}
-        <ul>
-            <li><a href="/create/">create</a></li>
-        </ul>
     </body>
     </html>
     '''
-# <li> 태그는 페이지에서 리스트로 표시하도록 함
 
 def index(request):
     article = '''
@@ -46,17 +42,5 @@ def read(request, id):
     return HttpResponse(HTMLTemplate(article))
 
 def create(request):
-    # placeholder - 텍스트 입력 박스에 흐린 글자로 쓰여지게 함
-    # <p> 태그는 단락을 나타냄
-    # textarea - 여러줄의 텍스트를 입력할 때 이용하는 태그
-    # submit 타입 - 제출 버튼
-    # form 태그로 감싸야 정보 보낼 수 있음, action은 보내고 싶은 서버의 path 작성
-    article = '''
-        <form action="/create/">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p><textarea name="body" placeholder="body"></textarea></p>
-            <p><input type="submit"></p>
-        </form>
-    '''
-    return HttpResponse(HTMLTemplate(article))
+    return HttpResponse('Create!')
 
