084a31d2c6379c057dc759a5bca727daa6854c2c
zibb03 <64014435+zibb03@users.noreply.github.com>
2022-07-20 03:24:32 +0900

    tag 객체화

--- myapp/views.py
+++ myapp/views.py
@@ -1,35 +1,46 @@
 from django.shortcuts import render, HttpResponse
 import random
 
+# Create your views here.
+
 topics = [
     {'id':1, 'title':'routing', 'body':'Routing is ..'},
     {'id':2, 'title':'view', 'body':'View is ..'},
     {'id':3, 'title':'Model', 'body':'Model is ..'},
 ]
 
-# Create your views here.
-def index(request):
-    # return HttpResponse('Welcome!')
-    # return HttpResponse('<h1>Random</h1>'+str(random.random()))
+def HTMLTemplate(articleTag):
     global topics
     ol = ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
-    return HttpResponse(f'''
-        <html>
-        <body>
-            <h1>Django</h1>
-            <ol>
-                {ol}
-            </ol>
-            <h2>Welcome</h2>
-            Hello, Django
-        </body>
-        </html>
-        ''')
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ul>
+            {ol}
+        </ul>
+        {articleTag}
+    </body>
+    </html>
+    '''
+
+def index(request):
+    article = '''
+    <h2>Welcome</h2> 
+    Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
+
+def read(request, id):
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
 
 def create(request):
     return HttpResponse('Create!')
 
-def read(request, id):
-    return HttpResponse('Read!'+id)
